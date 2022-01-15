1be0577c605ce71ccc7980899a74d9e4073ff9a8
Dusan-O <dusan.orescanin@gmail.com>
2022-01-15 20:45:29 +0100

    Modification of the label

--- Teki/ViewController.swift
+++ Teki/ViewController.swift
@@ -8,11 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-    var celebrities = ["le Steve Jobs", "le Zinedine Zidane", "la Madonna", "le Karl Lagarfeld", "la Scarlett Johansson"]
-    var activities = ["du dancefloor", "du barbecue", "de la surprise ratée", "des blagues lourdes", "de la raclette party"]
+    let celebrities = ["le Steve Jobs", "le Zinedine Zidane", "la Madonna", "le Karl Lagarfeld", "la Scarlett Johansson"]
+    let activities = ["du dancefloor", "du barbecue", "de la surprise ratée", "des blagues lourdes", "de la raclette party"]
     @IBOutlet weak var quoteLabel: UILabel!
     @IBAction func changeQuote(_ sender: Any) {
-        quoteLabel.text = "Le texte du label est modifié ! "
+        // Select random element among celebrtities
+        let randomIndex1 = Int(arc4random_uniform(UInt32(celebrities.count)))
+        let celebrity = celebrities[randomIndex1]
+        print(celebrity)
+        
+        // Select random element amoàng activities
+        let randomIndex2 = Int(arc4random_uniform(UInt32(activities.count)))
+        let activity = activities[randomIndex2]
+        print(activity)
+        
+        // Modify the label text
+        quoteLabel.text = " Le texte du label a été modifié !"
+        
     }
 }
     
