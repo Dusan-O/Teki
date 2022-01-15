0e8abf5beebf9c5960c78ab09e43905e0585925e
Dusan-O <dusan.orescanin@gmail.com>
2022-01-15 14:01:20 +0100

    Initial Commit

---
+++ Teki/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Teki
+//
+//  Created by Dusan Orescanin on 15/01/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
