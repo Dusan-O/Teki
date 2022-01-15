e42aab3df45a57d4868c2ed637829528eb3cbc30
Dusan-O <dusan.orescanin@gmail.com>
2022-01-15 15:20:27 +0100

    add bin + connection home page

--- Teki/ViewController.swift
+++ Teki/ViewController.swift
@@ -8,12 +8,8 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
+    @IBOutlet weak var quoteLabel: UILabel!
+    
 
 }
 
