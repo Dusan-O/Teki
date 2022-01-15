641c99c8b7da20013273e5384b52d5702391bbd5
Dusan-O <dusan.orescanin@gmail.com>
2022-01-15 20:11:47 +0100

    Connection of the bin Creation celebrities + sentences Arrays

--- Teki/ViewController.swift
+++ Teki/ViewController.swift
@@ -8,8 +8,13 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var celebrities = ["le Steve Jobs", "le Zinedine Zidane", "la Madonna", "le Karl Lagarfeld", "la Scarlett Johansson"]
+    var activities = ["du dancefloor", "du barbecue", "de la surprise ratée", "des blagues lourdes", "de la raclette party"]
     @IBOutlet weak var quoteLabel: UILabel!
+    @IBAction func changeQuote(_ sender: Any) {
+        quoteLabel.text = "Le texte du label est modifié ! "
+    }
+}
     
 
-}
 
